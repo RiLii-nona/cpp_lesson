9b18927ca43aa18d19198c87ff6a4df176a01589
RiLii-nona <ryoka0902.kg@gmail.com>
2023-05-14 20:48:48 +0900

    セットアップを行いました。

---
+++ build/CMakeFiles/phy_water.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phy_water.dir/src/main.cpp.o"
+  "CMakeFiles/phy_water.dir/src/main.cpp.o.d"
+  "phy_water"
+  "phy_water.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phy_water.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
